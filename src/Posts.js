4e1b05486d37cb87aec0d1ceecdc3e8ca53c0bf6
AAdler <amadler.dev@gmail.com>
2023-08-06 12:30:26 +0300

    add main post

--- src/Posts.js
+++ src/Posts.js
@@ -9,14 +9,31 @@ export default function Posts() {
 
     return (
         <div className="home">
+
+            <div className='main-section'>
+                {posts && posts.slice(0, 1).map((post, index) => (
+
+                <a href={JSON.stringify(post.link).slice(1,-1)} target='_blank' rel="noopener noreferrer">
+                    <div className='main-post' item xs={4} key={index}>
+                        <img className='image' alt='#' src={JSON.stringify(post.yoast_head_json.og_image[0].url).slice(1,-1)} />
+
+                        <div className=''>
+                            <div className='title' dangerouslySetInnerHTML={{__html: post.title.rendered}}></div>
+                            <div className='excerpt' dangerouslySetInnerHTML={{__html: post.excerpt.rendered}}></div>
+                        </div>
+                    </div>
+                </a>
+
+                )) }
+            </div>
+
             <div className='first-section' container spacing={2}>
-                {posts && posts.slice(0, 8).map((post, index) => (
+                {posts && posts.slice(1, 9).map((post, index) => (
 
                 <a href={JSON.stringify(post.link).slice(1,-1)} target='_blank' rel="noopener noreferrer">
                     <div className='post' item xs={4} key={index}>
-                        <div className='title' dangerouslySetInnerHTML={{__html: post.title.rendered}}></div>
-                            
                         <img className='image' alt='#' src={JSON.stringify(post.yoast_head_json.og_image[0].url).slice(1,-1)} />
+                        <div className='title' dangerouslySetInnerHTML={{__html: post.title.rendered}}></div>
                     </div>
                 </a>
 
@@ -29,9 +46,8 @@ export default function Posts() {
 
                 <a href={JSON.stringify(post.link).slice(1,-1)} target='_blank' rel="noopener noreferrer">
                     <div className='post' item xs={4} key={index}>
-                        <div className='title' dangerouslySetInnerHTML={{__html: post.title.rendered}}></div>
-                            
                         <img className='image' alt='#' src={JSON.stringify(post.yoast_head_json.og_image[0].url).slice(1,-1)} />
+                        <div className='title' dangerouslySetInnerHTML={{__html: post.title.rendered}}></div>        
                     </div>
                 </a>
 
